2c296ced7578e5c0e63ac90196fc4690cf8fea9a
Dennis Law <cmlaw1993@gmail.com>
2016-04-09 21:42:51 +0800

    Added new files for GPIO Driver.
    Added GPIO_DRV.c and GPIO_DRV.h

---
+++ GPIO_DRV.c
@@ -0,0 +1,9 @@
+
+/** @file GPIO_DRV.c
+ *  @brief GPIO Driver for the STM32F072RB.
+ *  @author Dennis Law
+ *  @date April 2016
+ */
+ 
+ #include "GPIO_DRV.h"
+ 
---
+++ GPIO_DRV.h
@@ -0,0 +1,13 @@
+
+/** @file GPIO_DRV.h
+ *  @brief GPIO Driver include file for the STM32F072RB.
+ *  @author Dennis Law
+ *  @date April 2016
+ */
+ 
+#ifndef GPIO_DRV_H
+#define GPIO_DRV_H
+
+#include "stm32f0xx.h"
+
+#endif	/* GPIO_DRV_H */

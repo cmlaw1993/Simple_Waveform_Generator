6b30bf80eab91b9ce6643179fa2620c6490eb909
Dennis Law <cmlaw1993@gmail.com>
2016-04-10 16:10:07 +0800

    Added new files for Timer Driver.
    New files created are TIMER_DRV.c and TIMER_DRV.h

---
+++ TIMER_DRV.c
@@ -0,0 +1,9 @@
+
+/** @file TIMER_DRV.c
+ *  @brief TIMER Driver for the STM32F072RB.
+ *  @author Dennis Law
+ *  @date April 2016
+ */
+ 
+ #include "TIMER_DRV.h"
+ 
---
+++ TIMER_DRV.h
@@ -0,0 +1,13 @@
+
+/** @file TIMER_DRV.h
+ *  @brief TIMER Driver include file for the STM32F072RB.
+ *  @author Dennis Law
+ *  @date April 2016
+ */
+ 
+#ifndef TIMER_DRV_H
+#define TIMER_DRV_H
+
+#include "stm32f0xx.h"
+
+#endif	/* TIMER_DRV_H */
